837123073e597e4ca0df7ab64b65599d6962f4f0
Thales Ribeiro <thalessr@gmail.com>
2014-08-10 16:59:37 -0300

    Getting API token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,17 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  def setup_api
+  	begin
+      api = RdSales::API.new('client_secret', 
+		              "client_id",
+		              'username',
+		              'password')
+      @token = api.get_token
+    rescue => e
+      flash.now[:alert] = 'There was a problem connecting to SalesForce api'
+    end
+    @token
+  end
+
 end
